1f8d567ee92df0753fc668eaf1de61747e920d80
MatiasRetamales <matiasretamalescarrera@gmail.com>
2026-06-16 18:11:46 -0400

    Se agregan los campos de propina al modelo de PEDIDO

--- pedidos/models.py
+++ pedidos/models.py
@@ -6,25 +6,30 @@ from carta.models import Producto
 
 class Pedido(models.Model):
     ESTADOS = (
-    ('pendiente', 'Pendiente'),
-    ('en_preparacion', 'En Cocina'),  # Cambiamos solo lo que ve el garzón
-    ('listo', 'Para Entregar'),      # El código sigue buscando 'listo'
-    ('entregado', 'Servido'),        # Pero el garzón ve 'Servido'
-    ('cancelado', 'Cancelado'),
-    ('pagado', 'Pagado'),            
-)
-    
+        ('pendiente', 'Pendiente'),
+        ('en_preparacion', 'En Cocina'),
+        ('listo', 'Para Entregar'),
+        ('entregado', 'Servido'),
+        ('cancelado', 'Cancelado'),
+        ('pagado', 'Pagado'),
+    )
 
-    mesa = models.ForeignKey(Mesa, on_delete=models.SET_NULL, null=True, blank=True) # Relación con Mesa
-    garzon = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True) # Relación con Usuario (Garzón)
+    mesa = models.ForeignKey(Mesa, on_delete=models.SET_NULL, null=True, blank=True)
+    garzon = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     estado = models.CharField(max_length=20, choices=ESTADOS, default='pendiente')
     fecha = models.DateTimeField(auto_now_add=True)
     fecha_pago = models.DateTimeField(null=True, blank=True)
     enviado_a_cocina = models.DateTimeField(null=True, blank=True)
-    total_envios = models.PositiveIntegerField(default=0) #total de envios a cocina para este pedido
-    restaurante = models.ForeignKey("core.Restaurante", on_delete=models.CASCADE) # Relación con Restaurante
-    nombre_cliente = models.CharField(max_length=100, blank=True, null=True) # Nombre del cliente para pedidos sin mesa
+    total_envios = models.PositiveIntegerField(default=0)
+    restaurante = models.ForeignKey("core.Restaurante", on_delete=models.CASCADE)
+    nombre_cliente = models.CharField(max_length=100, blank=True, null=True)
+
     total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    propina_aplicada = models.BooleanField(default=False)
+    monto_propina = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def total_con_propina(self):
+        return self.total + self.monto_propina
 
     def __str__(self):
         return f"Pedido {self.id} - Mesa {self.mesa.numero}"
