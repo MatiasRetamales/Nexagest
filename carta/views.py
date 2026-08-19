f73826e159f8ae8bdd9a16aa8dbdcbe81d4f8fc5
MatiasRetamales <matiasretamalescarrera@gmail.com>
2026-08-19 16:35:09 -0400

    CAMBIOS MENORES

--- carta/views.py
+++ carta/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from core.models import Caja, Restaurante
+from core.models import Restaurante
 from .models import Categoria
+from administracion.models import Caja
 
 
 def carta_publica(request, restaurante_id):
@@ -20,6 +21,8 @@ def carta_publica(request, restaurante_id):
         esta_abierta=True
     ).first()
 
+    caja_abierta = caja is not None
+
     return render(
         request,
         'carta/carta_publica.html',
@@ -27,5 +30,6 @@ def carta_publica(request, restaurante_id):
             'restaurante': restaurante,
             'categorias': categorias,
             'caja': caja,
+            'caja_abierta': caja_abierta,
         }
     )
