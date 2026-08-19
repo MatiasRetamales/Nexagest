61464bf7cb617465d2bdc3557e8078a9b8eea363
MatiasRetamales <matiasretamalescarrera@gmail.com>
2026-08-19 15:13:09 -0400

    cambio en carta views

--- carta/views.py
+++ carta/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render, get_object_or_404
-from core.models import Restaurante
+from core.models import Caja, Restaurante
 from .models import Categoria
 
 
 def carta_publica(request, restaurante_id):
-    restaurante = get_object_or_404(Restaurante, id=restaurante_id)
+
+    restaurante = get_object_or_404(
+        Restaurante,
+        id=restaurante_id
+    )
 
     categorias = Categoria.objects.filter(
-    restaurante=restaurante,
-    producto__disponibilidad='disponible'
-    ).distinct() 
+        restaurante=restaurante,
+        producto__disponibilidad='disponible'
+    ).distinct()
 
-    return render(request, 'carta/carta_publica.html', {
-        'restaurante': restaurante,
-        'categorias': categorias,
-    })
+    caja = Caja.objects.filter(
+        restaurante=restaurante,
+        esta_abierta=True
+    ).first()
+
+    return render(
+        request,
+        'carta/carta_publica.html',
+        {
+            'restaurante': restaurante,
+            'categorias': categorias,
+            'caja': caja,
+        }
+    )
