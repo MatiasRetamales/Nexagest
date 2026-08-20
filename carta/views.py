9ad2060e4f1723be6a12947bb785ae0c3219dc54
MatiasRetamales <matiasretamalescarrera@gmail.com>
2026-08-20 18:23:37 -0400

    prueba 15

--- carta/views.py
+++ carta/views.py
@@ -4,46 +4,36 @@ from .models import Categoria
 from administracion.models import Caja
 
 
-import traceback
-
-
 def carta_publica(request, restaurante_id):
+
 
     restaurante = get_object_or_404(
         Restaurante,
         id=restaurante_id
     )
 
+
     categorias = Categoria.objects.filter(
         restaurante=restaurante
     )
+
 
     caja = Caja.objects.filter(
         restaurante=restaurante,
         esta_abierta=True
     ).first()
 
+
     caja_abierta = caja is not None
 
-    try:
 
-        return render(
-            request,
-            "carta/carta_publica.html",
-            {
-                "restaurante": restaurante,
-                "categorias": categorias,
-                "caja": caja,
-                "caja_abierta": caja_abierta,
-            }
-        )
-
-    except Exception:
-
-        print("\n========== ERROR CARTA ==========")
-
-        traceback.print_exc()
-
-        print("=================================\n")
-
-        raise
+    return render(
+        request,
+        "carta/carta_publica.html",
+        {
+            "restaurante": restaurante,
+            "categorias": categorias,
+            "caja": caja,
+            "caja_abierta": caja_abierta,
+        }
+    )
