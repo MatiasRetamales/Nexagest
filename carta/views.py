d193fb471bec196f047f096c8052cc34534a8954
MatiasRetamales <matiasretamalescarrera@gmail.com>
2026-08-20 18:18:41 -0400

    prueba 13

--- carta/views.py
+++ carta/views.py
@@ -6,31 +6,26 @@ from administracion.models import Caja
 
 def carta_publica(request, restaurante_id):
 
-    print("1 - ENTRANDO A VISTA")
 
     restaurante = get_object_or_404(
         Restaurante,
         id=restaurante_id
     )
 
-    print("2 - RESTAURANTE:", restaurante)
 
     categorias = Categoria.objects.filter(
         restaurante=restaurante
     )
 
-    print("3 - CATEGORIAS:", list(categorias))
 
     caja = Caja.objects.filter(
         restaurante=restaurante,
         esta_abierta=True
     ).first()
 
-    print("4 - CAJA:", caja)
 
     caja_abierta = caja is not None
 
-    print("5 - ANTES RENDER")
 
     return render(
         request,
